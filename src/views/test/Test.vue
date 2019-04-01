5a77f8a74205590b1e55227c29ae513f6ec49659
徐伟(10027568) <xuw-i@glodon.com>
2019-04-01 19:48:47 +0800

    初始化vue+cesium模板

---
+++ src/views/test/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="cesiumBox"></div>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                
+            }
+        },
+        mounted () {
+            new this.Cesium.Viewer('cesiumBox')
+        }
+    };
+</script>
+<style lang="less">
+    @import url("./test.less");
+</style>
